e2a4628fe05dfe7a51ccfda9aac1a9047b1e5ac7
NaUbitejZiemi <naubitejziemi@op.pl>
2024-11-03 15:51:00 +0100

    navbar creation wip save

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,46 @@
+import React, { useState } from "react";
+import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [nav, setnav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setnav(true);
+    } else {
+      setnav(false);
+    }
+  };
+  window.addEventListener("scroll", changeBackground);
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/gallery">Gallery</Link>
-    </div>
+    <nav className={nav ? "nav active" : "nav"}>
+      <Link to="#" className="logo">
+        <img src={logo} alt="" />
+      </Link>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-btn" for="menu-btn">
+        <span className="nav-icon"></span>
+      </label>
+      <ul className="menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Info</Link>
+        </li>
+        <li>
+          <Link to="/">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 export default Navbar;
